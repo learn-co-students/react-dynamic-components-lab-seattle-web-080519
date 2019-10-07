081c0f9a9d3afbcff7cefc581bdc06ab9952e0c6
AKhan-CIR <akhan@codeinreach.com>
2019-10-07 16:49:40 -0700

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,14 @@
-// add Comment component here
+import React from "react";
+
+const Comment = props => {
+  return (
+    <li
+      className="comment"
+      style={{ backgroundColor: "#ccff00", color: "teal" }}
+    >
+      {props.commentText}
+    </li>
+  );
+};
+
+export default Comment;
